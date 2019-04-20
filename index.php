1e2dcb6c4db6d32ed5f26baa808f25cfa90e4786
inc002 <inc002@gmail.com>
2019-04-20 23:39:13 +0200

    add instagram

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ include('resources/simple_html_dom.php');
 
 //twitter : ProfileAvatar-image 
 //telegram : tgme_page_photo_image
+//instagram : _6q-tv
 
 $tabSN = array ();
 $tabSN[0]['name'] = 'twitter';
@@ -14,9 +15,12 @@ $tabSN[0]['url'] = 'https://twitter.com/';
 $tabSN[1]['name'] = 'telegram';
 $tabSN[1]['class_img'] = 'tgme_page_photo_image';
 $tabSN[1]['url'] = 'https://t.me/';
+$tabSN[2]['name'] = 'instagram';
+$tabSN[2]['class_img'] = '_6q-tv';
+$tabSN[2]['url'] = 'https://www.instagram.com/';
 $folderGIF = 'GIF/';
 $error = false;
-if (!isset ($_GET['n']) OR empty($_GET['n']) OR !is_numeric($_GET['n']) OR $_GET['n']>1){
+if (!isset ($_GET['n']) OR empty($_GET['n']) OR !is_numeric($_GET['n']) OR $_GET['n']>2){
 //choose ID of Social Network 
 // 0 : twitter
 // 1 : telegram
@@ -37,11 +41,51 @@ function pre($var){
 	echo "</pre>";
 }
 
-function printOut($urlGIF,$idSN,$tabSN,$s,$error){
+function getTop50TwitterAccount(){
+	//wikitable sortable jquery-tablesorter
+	$url = 'https://en.wikipedia.org/wiki/List_of_most-followed_Twitter_accounts';
+	$html = file_get_html($url);
+	if ($html){
+		foreach($html->find('"table.wikitable sortable" tr') as $element){
+			foreach($element->find('td') as $cell) {
+				// push the cell's text to the array
+				if (substr($cell->plaintext,0,1)==='@'){
+					$tabTwittos[] = $cell->plaintext;
+				} 
+			}
+		}
+		return $tabTwittos;
+	}
+	return false;
+}
+
+function randomGIF($tabName){
+	$nbRandom = rand(2,8);
+	$nbTabName = count($tabName);	
+	$string = '';
+	for ($i=0;$i<$nbRandom;$i++){
+		$valRandom = rand($i,$nbTabName);
+		$string .= $tabName[$valRandom].' ';
+	}
+	return $string;
+}
+
+//pre(randomGIF(getTop50TwitterAccount()));
+
+function printOut($urlGIF,$idSN,$tabSN,$s,$listAccount,$error){
+	if (empty($listAccount)){
+		$account = explode(' ',$s);
+		for ($i=0;$i<count($account);$i++){
+			$listAccount .= '<a href="'.$tabSN[$idSN]['url'].$account[$i].'">@'.$account[$i].'</a> ';
+		}
+		//
+		//$listAccount = '@'.$listAccount;
+	}
 	$display = '	<br /><a href="'.$urlGIF.'">Download GIF (right click : Save target as...)</a>
 					<br /><br /><img src="'.$urlGIF.'">
 					<br /><br />Images from '.$tabSN[$idSN]['name'].
-					'<br /><br /><a href="#" onclick="window.history.back();">Back</a>';
+					'<br /><br />Avatar of : '.$listAccount.'&nbsp;
+					<br /><br /><a href="#" onclick="window.history.back();">Back</a>';
 	
 	if (empty($s) or !isset($s)){
 		$display = '
@@ -53,7 +97,15 @@ function printOut($urlGIF,$idSN,$tabSN,$s,$error){
 		<label for="s1">Twitter</label>	
 		<input type="radio" name="n" id="n1" value="1">
 		<label for="s1">Telegram</label>
-		<input type="submit"></form>'; 
+		<input type="radio" name="n" id="n2" value="2">
+		<label for="s1">Instagram</label>
+		<br />
+		<br />
+		<input type="submit"></form>
+		<form>
+		<a href="?r=0">Random Top 50 twitter account</a>
+		</form>
+		'; 
 	}
 	if ($error){
 		$display = $error;
@@ -71,6 +123,11 @@ function printOut($urlGIF,$idSN,$tabSN,$s,$error){
 	
 		return $out;
 } 
+if (isset($_GET['r']) AND is_numeric($_GET['r']))	{
+	$_GET['s'] = randomGIF(getTop50TwitterAccount());
+	$_GET['n'] = $_GET['r'];
+	
+}
 
 $nameGIF = $folderGIF.md5($_GET['s'].$_GET['n']).".gif";
 $startUrl = !empty($_SERVER['HTTPS']) ? 'https://' : 'http://' ;
@@ -80,14 +137,24 @@ if (!file_exists($nameGIF)){
 	//Get url of avatar pictures
 	$resTabWords = getWords($_GET['s']);
 	$nbWords = count($resTabWords);
+	$listAccount = '';
 	for ($i=0;$i<$nbWords;$i++){
 		// Create DOM from URL or file
-		$url = $tabSN[$idSN]['url'].$resTabWords[$i];
+		$url = $tabSN[$idSN]['url'].$resTabWords[$i].'';
 		if (!empty($resTabWords[$i])){
 			$html = file_get_html($url);
 			if ($html){
-				foreach($html->find('img.'.$tabSN[$idSN]['class_img']) as $element){
-					$frames[] = file_get_contents($element->src);
+				$resTabWords[$i] = str_replace('@','',$resTabWords[$i]);
+				$listAccount .= '<a href="'.$url.'">@'.$resTabWords[$i].'</a> ';
+				if ($idSN == 2){
+					//meta property="og:image"
+					foreach($html->find('meta[property=og:image]') as $element){
+						$frames[] = $element->content;
+					}
+				}else{
+					foreach($html->find('img.'.$tabSN[$idSN]['class_img']) as $element){
+						$frames[] = file_get_contents($element->src);
+					}
 				}
 			}
 		}
@@ -102,7 +169,7 @@ if (!file_exists($nameGIF)){
 	}
 	
 }
-echo printOut($urlGIF,$idSN,$tabSN,$_GET['s'],$error);
+echo printOut($urlGIF,$idSN,$tabSN,$_GET['s'],$listAccount,$error);
 
 
 

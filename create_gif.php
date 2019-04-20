90abc99cb6df0e5009e1c23c2fcf7ee24c046e97
inc002 <arnaud@loumy.fr>
2019-04-20 17:00:47 +0200

    first release

---
+++ create_gif.php
@@ -0,0 +1,14 @@
+<?php
+include('animGif.php');
+// Use an array containing file paths, resource vars (initialized with imagecreatefromXXX), 
+// image URLs or binary image data.
+// Optionally: set different durations (in 1/100s units) for each frame
+$durations = array(100);
+$anim = new GifCreator\AnimGif();
+$anim->create($frames, $durations);
+$gif = $anim->get();
+header("Content-type: image/gif");
+echo $gif;
+exit;
+
+?>
---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+error_reporting(E_ALL);
+
+include('simple_html_dom.php');
+
+function getWords($sentence){
+	$sentencePlus = str_replace(' ','+',$sentence);
+	$tabWords = explode('+',$sentencePlus);
+	return $tabWords;
+}
+
+function pre($var){
+	echo "<pre>";
+	var_dump($var);
+	echo "</pre>";
+}
+
+$resTabWords = getWords($_GET['s']);
+$nbWords = count($resTabWords);
+
+//twitter : ProfileAvatar-image 
+//telegram : tgme_page_photo_image
+//facebook : img dans div profilePicThumb 
+
+
+$tabSN = array ();
+$tabSN[0]['name'] = 'twitter';
+$tabSN[0]['class_img'] = 'ProfileAvatar-image';
+$tabSN[0]['url'] = 'https://twitter.com/';
+$tabSN[1]['name'] = 'telegram';
+$tabSN[1]['class_img'] = 'tgme_page_photo_image';
+$tabSN[1]['url'] = 'https://t.me/';
+
+
+$idSN = 0;
+
+for ($i=0;$i<$nbWords;$i++){
+	// Create DOM from URL or file
+	$url = $tabSN[$idSN]['url'].$resTabWords[$i];
+	$html = file_get_html($url);
+	foreach($html->find('img.'.$tabSN[$idSN]['class_img']) as $element){
+		$frames[] = file_get_contents($element->src);
+	}
+}
+
+include ('create_gif.php');
+
+
+
+
+
+?>
